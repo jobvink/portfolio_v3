c2dc98d4d29aaa09a34cdbed725e4ef7ef4844fb
Job Vink <jmcvink@gmail.com>
2017-12-02 20:00:20 +0100

    portfolio en timeline af

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var navbackground = function () {
+    if ($(window).scrollTop() > 150 ) {
+        $('nav').css('background', 'black');
+    } else {
+        $('nav').css('background', 'transparent');
+    }
+};
+navbackground();
+$(window).scroll(function () {
+    navbackground()
+});
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 75
+                }, 1000);
+            }
+        }
+    });
