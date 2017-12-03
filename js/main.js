1f7355c5a0dd0fa571e9c5c855d1fa456daffed4
Job Vink <jmcvink@gmail.com>
2017-12-03 14:03:51 +0100

    responsive update en content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var navbackground = function () {
-    if ($(window).scrollTop() > 150 ) {
+    if ($(window).scrollTop() > 150) {
         $('nav').css('background', 'black');
     } else {
         $('nav').css('background', 'transparent');
@@ -10,13 +10,26 @@ $(window).scroll(function () {
     navbackground()
 });
 
+// $('#modal1').modal('open');
+$('.portfolios').on('click', '.portfolio', function () {
+    var modal = $(this).data('modal');
+    console.log(modal);
+    var $modal = $(modal);
+    $modal.addClass('active');
+    var $body = $('body');
+    $body.addClass('modal-open');
+    $modal.find('.close').on('click', function () {
+        $body.removeClass('modal-open');
+        $modal.removeClass('active');
+    })
+});
 
 // Select all links with hashes
 $('a[href*="#"]')
 // Remove links that don't actually link to anything
     .not('[href="#"]')
     .not('[href="#0"]')
-    .click(function(event) {
+    .click(function (event) {
         // On-page links
         if (
             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
